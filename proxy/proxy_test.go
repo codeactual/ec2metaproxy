bbdeab977527759dcfff3459fc46f6e5ddf0b28f
David Smith <codeactual@gmail.com>
2016-12-07 07:27:51 +0000

    test: refactor response coverage

--- proxy/proxy_test.go
+++ proxy/proxy_test.go
@@ -1,9 +1,10 @@
 package proxy
 
 import (
+	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -16,30 +17,10 @@ func TestResponse(t *testing.T) {
 		res, _, err := stubRequest(defaultPathSpec, defaultPathReq, config, stsSvc, containerSvc, defaultIP)
 		fatalOnErr(t, err)
 
-		if res.Code != 200 {
-			t.Fatalf("expected HTTP code 200, got %d", res.Code)
-		}
-
-		var body metadataCredentials
-		err = json.NewDecoder(res.Body).Decode(&body)
-		fatalOnErr(t, err)
-
-		expectedCreds := defaultCreds()
-		stringsEqual(t, [][2]string{
-			[2]string{"Success", body.Code},
-			[2]string{*expectedCreds.AccessKeyId, body.AccessKeyID},
-			[2]string{"AWS-HMAC", body.Type},
-			[2]string{*expectedCreds.SecretAccessKey, body.SecretAccessKey},
-			[2]string{*expectedCreds.SessionToken, body.Token},
-			[2]string{stsSvc.output.Credentials.Expiration.String(), body.Expiration.String()},
-		})
-
-		if *stsSvc.input.RoleArn != config.AliasToARN["noperms"] {
-			t.Fatalf("expected assumed role to be [%s], instead [%s]", config.AliasToARN["noperms"], *stsSvc.input.RoleArn)
-		}
-		if stsSvc.input.Policy != nil {
-			t.Fatalf("expected no policy, got [%s]", *stsSvc.input.Policy)
-		}
+		responseCodeIs(t, res, 200)
+		credsEqualDefaults(t, res.Body, stsSvc)
+		assumeRoleAliasIs(t, "noperms", config, stsSvc)
+		assumeRolePolicyIsNil(t, stsSvc)
 	})
 
 	t.Run("should detect alias mismatch with request path", func(t *testing.T) {
@@ -50,22 +31,10 @@ func TestResponse(t *testing.T) {
 		res, _, err := stubRequest(defaultPathSpec, defaultPathReqBase+"/invalid", config, stsSvc, containerSvc, defaultIP)
 		fatalOnErr(t, err)
 
-		if res.Code != 404 {
-			t.Fatalf("expected HTTP code 404, got %d", res.Code)
-		}
-
-		bodyBytes, err := ioutil.ReadAll(res.Body)
-		fatalOnErr(t, err)
-		if len(bodyBytes) != 0 {
-			t.Fatalf("expected empty body, got [%s]", string(bodyBytes))
-		}
-
-		if *stsSvc.input.RoleArn != config.AliasToARN["noperms"] {
-			t.Fatalf("expected assumed role to be [%s], instead [%s]", config.AliasToARN["noperms"], *stsSvc.input.RoleArn)
-		}
-		if stsSvc.input.Policy != nil {
-			t.Fatalf("expected no policy, got [%s]", *stsSvc.input.Policy)
-		}
+		responseCodeIs(t, res, 404)
+		bodyIsEmpty(t, res.Body)
+		assumeRoleAliasIs(t, "noperms", config, stsSvc)
+		assumeRolePolicyIsNil(t, stsSvc)
 	})
 
 	t.Run("should detect request path without role", func(t *testing.T) {
@@ -76,27 +45,15 @@ func TestResponse(t *testing.T) {
 		res, _, err := stubRequest(defaultPathSpec, defaultPathReqBase+"/", config, stsSvc, containerSvc, defaultIP)
 		fatalOnErr(t, err)
 
-		if res.Code != 200 {
-			t.Fatalf("expected HTTP code 200, got %d", res.Code)
+		responseCodeIs(t, res, 200)
+		body := bodyIsNonEmpty(t, res.Body)
+
+		if body != defaultRoleARNFriendlyName {
+			t.Fatalf("expected role ARN [%s], got [%s]", defaultRoleARNFriendlyName, body)
 		}
 
-		bodyBytes, err := ioutil.ReadAll(res.Body)
-		fatalOnErr(t, err)
-		if len(bodyBytes) == 0 {
-			t.Fatal("expected non-empty body")
-		}
-		bodyBytesStr := string(bodyBytes)
-
-		if bodyBytesStr != defaultRoleARNFriendlyName {
-			t.Fatalf("expected role ARN [%s], got [%s]", defaultRoleARNFriendlyName, bodyBytesStr)
-		}
-
-		if *stsSvc.input.RoleArn != config.AliasToARN["noperms"] {
-			t.Fatalf("expected assumed role to be [%s], instead [%s]", config.AliasToARN["noperms"], *stsSvc.input.RoleArn)
-		}
-		if stsSvc.input.Policy != nil {
-			t.Fatalf("expected no policy, got [%s]", *stsSvc.input.Policy)
-		}
+		assumeRoleAliasIs(t, "noperms", config, stsSvc)
+		assumeRolePolicyIsNil(t, stsSvc)
 	})
 
 	t.Run("should apply default role", func(t *testing.T) {
@@ -107,30 +64,10 @@ func TestResponse(t *testing.T) {
 		res, _, err := stubRequest(defaultPathSpec, defaultPathReq, config, stsSvc, containerSvc, ipWithNoLabels)
 		fatalOnErr(t, err)
 
-		if res.Code != 200 {
-			t.Fatalf("expected HTTP code 200, got %d", res.Code)
-		}
-
-		var body metadataCredentials
-		err = json.NewDecoder(res.Body).Decode(&body)
-		fatalOnErr(t, err)
-
-		expectedCreds := defaultCreds()
-		stringsEqual(t, [][2]string{
-			[2]string{"Success", body.Code},
-			[2]string{*expectedCreds.AccessKeyId, body.AccessKeyID},
-			[2]string{"AWS-HMAC", body.Type},
-			[2]string{*expectedCreds.SecretAccessKey, body.SecretAccessKey},
-			[2]string{*expectedCreds.SessionToken, body.Token},
-			[2]string{stsSvc.output.Credentials.Expiration.String(), body.Expiration.String()},
-		})
-
-		if *stsSvc.input.RoleArn != config.AliasToARN["noperms"] {
-			t.Fatalf("expected assumed role to be [%s], instead [%s]", config.AliasToARN["noperms"], *stsSvc.input.RoleArn)
-		}
-		if stsSvc.input.Policy != nil {
-			t.Fatalf("expected no policy, got [%s]", *stsSvc.input.Policy)
-		}
+		responseCodeIs(t, res, 200)
+		credsEqualDefaults(t, res.Body, stsSvc)
+		assumeRoleAliasIs(t, "noperms", config, stsSvc)
+		assumeRolePolicyIsNil(t, stsSvc)
 	})
 
 	t.Run("should apply default policy", func(t *testing.T) {
@@ -143,30 +80,10 @@ func TestResponse(t *testing.T) {
 		res, _, err := stubRequest(defaultPathSpec, defaultPathReq, config, stsSvc, containerSvc, ipWithNoLabels)
 		fatalOnErr(t, err)
 
-		if res.Code != 200 {
-			t.Fatalf("expected HTTP code 200, got %d", res.Code)
-		}
-
-		var body metadataCredentials
-		err = json.NewDecoder(res.Body).Decode(&body)
-		fatalOnErr(t, err)
-
-		expectedCreds := defaultCreds()
-		stringsEqual(t, [][2]string{
-			[2]string{"Success", body.Code},
-			[2]string{*expectedCreds.AccessKeyId, body.AccessKeyID},
-			[2]string{"AWS-HMAC", body.Type},
-			[2]string{*expectedCreds.SecretAccessKey, body.SecretAccessKey},
-			[2]string{*expectedCreds.SessionToken, body.Token},
-			[2]string{stsSvc.output.Credentials.Expiration.String(), body.Expiration.String()},
-		})
-
-		if *stsSvc.input.RoleArn != config.AliasToARN["noperms"] {
-			t.Fatalf("expected assumed role to be [%s], instead [%s]", config.AliasToARN["noperms"], *stsSvc.input.RoleArn)
-		}
-		if *stsSvc.input.Policy != defaultPolicy {
-			t.Fatalf("expected custom policy [%s], got [%s]", defaultPolicy, *stsSvc.input.Policy)
-		}
+		responseCodeIs(t, res, 200)
+		credsEqualDefaults(t, res.Body, stsSvc)
+		assumeRoleAliasIs(t, "noperms", config, stsSvc)
+		assumeRolePolicyIs(t, defaultPolicy, stsSvc)
 	})
 
 	t.Run("should support custom labels", func(t *testing.T) {
@@ -177,30 +94,10 @@ func TestResponse(t *testing.T) {
 		res, _, err := stubRequest(defaultPathSpec, defaultPathReqBase+"/"+dbRoleARNFriendlyName, config, stsSvc, containerSvc, ipWithAllLabels)
 		fatalOnErr(t, err)
 
-		if res.Code != 200 {
-			t.Fatalf("expected HTTP code 200, got %d", res.Code)
-		}
-
-		var body metadataCredentials
-		err = json.NewDecoder(res.Body).Decode(&body)
-		fatalOnErr(t, err)
-
-		expectedCreds := defaultCreds()
-		stringsEqual(t, [][2]string{
-			[2]string{"Success", body.Code},
-			[2]string{*expectedCreds.AccessKeyId, body.AccessKeyID},
-			[2]string{"AWS-HMAC", body.Type},
-			[2]string{*expectedCreds.SecretAccessKey, body.SecretAccessKey},
-			[2]string{*expectedCreds.SessionToken, body.Token},
-			[2]string{stsSvc.output.Credentials.Expiration.String(), body.Expiration.String()},
-		})
-
-		if *stsSvc.input.RoleArn != config.AliasToARN["db"] {
-			t.Fatalf("expected assumed role to be [%s], instead [%s]", config.AliasToARN["db"], *stsSvc.input.RoleArn)
-		}
-		if *stsSvc.input.Policy != defaultCustomPolicy {
-			t.Fatalf("expected custom policy [%s], got [%s]", defaultCustomPolicy, *stsSvc.input.Policy)
-		}
+		responseCodeIs(t, res, 200)
+		credsEqualDefaults(t, res.Body, stsSvc)
+		assumeRoleAliasIs(t, "db", config, stsSvc)
+		assumeRolePolicyIs(t, defaultCustomPolicy, stsSvc)
 	})
 
 	t.Run("should support no selected defaults", func(t *testing.T) {
@@ -213,21 +110,72 @@ func TestResponse(t *testing.T) {
 		res, _, err := stubRequest(defaultPathSpec, defaultPathReq, config, stsSvc, containerSvc, ipWithNoLabels)
 		fatalOnErr(t, err)
 
-		if res.Code != 404 {
-			t.Fatalf("expected HTTP code 404, got %d", res.Code)
-		}
-
-		bodyBytes, err := ioutil.ReadAll(res.Body)
-		fatalOnErr(t, err)
-		if len(bodyBytes) != 0 {
-			t.Fatalf("expected empty body, got [%s]", string(bodyBytes))
-		}
-
-		if *stsSvc.input.RoleArn != "" {
-			t.Fatalf("expected assumed role to be empty, instead [%s]", *stsSvc.input.RoleArn)
-		}
-		if stsSvc.input.Policy != nil {
-			t.Fatalf("expected no policy, got [%s]", *stsSvc.input.Policy)
-		}
+		responseCodeIs(t, res, 404)
+		bodyIsEmpty(t, res.Body)
+		assumeRoleAliasIsEmpty(t, stsSvc)
+		assumeRolePolicyIsNil(t, stsSvc)
 	})
+}
+
+func credsEqualDefaults(t *testing.T, body *bytes.Buffer, stsSvc *assumeRoleStub) {
+	var c metadataCredentials
+	err := json.NewDecoder(body).Decode(&c)
+	fatalOnErr(t, err)
+
+	expectedCreds := defaultCreds()
+	stringsEqual(t, [][2]string{
+		[2]string{"Success", c.Code},
+		[2]string{*expectedCreds.AccessKeyId, c.AccessKeyID},
+		[2]string{"AWS-HMAC", c.Type},
+		[2]string{*expectedCreds.SecretAccessKey, c.SecretAccessKey},
+		[2]string{*expectedCreds.SessionToken, c.Token},
+		[2]string{stsSvc.output.Credentials.Expiration.String(), c.Expiration.String()},
+	})
+}
+
+func bodyIsEmpty(t *testing.T, body *bytes.Buffer) {
+	bodyBytes, err := ioutil.ReadAll(body)
+	fatalOnErr(t, err)
+	if len(bodyBytes) != 0 {
+		t.Fatalf("expected empty body, got [%s]", string(bodyBytes))
+	}
+}
+
+func bodyIsNonEmpty(t *testing.T, body *bytes.Buffer) string {
+	bodyBytes, err := ioutil.ReadAll(body)
+	fatalOnErr(t, err)
+	if len(bodyBytes) == 0 {
+		t.Fatal("expected non-empty body")
+	}
+	return string(bodyBytes)
+}
+
+func responseCodeIs(t *testing.T, res *httptest.ResponseRecorder, expected int) {
+	if res.Code != expected {
+		t.Fatalf("expected HTTP code %d, got %d", expected, res.Code)
+	}
+}
+
+func assumeRoleAliasIsEmpty(t *testing.T, stsSvc *assumeRoleStub) {
+	if *stsSvc.input.RoleArn != "" {
+		t.Fatalf("expected assume role to be empty, instead [%s]", *stsSvc.input.RoleArn)
+	}
+}
+
+func assumeRoleAliasIs(t *testing.T, alias string, config Config, stsSvc *assumeRoleStub) {
+	if *stsSvc.input.RoleArn != config.AliasToARN[alias] {
+		t.Fatalf("expected assume role to be [%s] with alias [%s], instead [%s]", config.AliasToARN[alias], alias, *stsSvc.input.RoleArn)
+	}
+}
+
+func assumeRolePolicyIs(t *testing.T, policy string, stsSvc *assumeRoleStub) {
+	if *stsSvc.input.Policy != policy {
+		t.Fatalf("expected policy [%s], got [%s]", policy, *stsSvc.input.Policy)
+	}
+}
+
+func assumeRolePolicyIsNil(t *testing.T, stsSvc *assumeRoleStub) {
+	if stsSvc.input.Policy != nil {
+		t.Fatalf("expected no policy, got [%s]", *stsSvc.input.Policy)
+	}
 }
